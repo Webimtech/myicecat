8d3c316fdbda0a87fb486c81a4832fe76eb3a6a6
Webimtech <66035639+Webimtech@users.noreply.github.com>
2020-06-10 17:12:32 +0200

    decommenter models.py

--- models/models.py
+++ models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class myicecat(models.Model):
-#     _name = 'myicecat.myicecat'
-#     _description = 'myicecat.myicecat'
+class myicecat(models.Model):
+    _name = 'myicecat.myicecat'
+    _description = 'myicecat.myicecat'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 #
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100

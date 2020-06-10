f37ab25d20faeee3eb78b95854f5253453fd1185
Webimtech <66035639+Webimtech@users.noreply.github.com>
2020-06-10 17:10:17 +0200

    decommenter controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Myicecat(http.Controller):
-#     @http.route('/myicecat/myicecat/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Myicecat(http.Controller):
+    @http.route('/myicecat/myicecat/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/myicecat/myicecat/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('myicecat.listing', {
-#             'root': '/myicecat/myicecat',
-#             'objects': http.request.env['myicecat.myicecat'].search([]),
-#         })
+    @http.route('/myicecat/myicecat/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('myicecat.listing', {
+            'root': '/myicecat/myicecat',
+            'objects': http.request.env['myicecat.myicecat'].search([]),
+        })
 
-#     @http.route('/myicecat/myicecat/objects/<model("myicecat.myicecat"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('myicecat.object', {
-#             'object': obj
-#         })
+    @http.route('/myicecat/myicecat/objects/<model("myicecat.myicecat"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('myicecat.object', {
+            'object': obj
+        })

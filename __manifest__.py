e20e6c4e29c16d73eb5c6ebc9184c912fb5c85c4
Webimtech <66035639+Webimtech@users.noreply.github.com>
2020-06-10 17:13:28 +0200

    Categorie Extra Tools

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Extra Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
